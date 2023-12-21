fb8f5ee3c5d7feee7ca4ef27bd0774f95a925308
Omar Mokhtar <omar.m.awad1212@gmail.com>
2023-12-21 20:04:54 +0200

    initial commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,45 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * is_digit - Check if a string is a valid positive integer
+ * @str: The string to check
+ * Return: 1 if it's a digit, 0 otherwise
+ */
 int is_digit(char *str)
 {
 	while (*str)
 	{
 		if (*str < '0' || *str > '9')
-			return 0;
+		{
+			return (0);
+		}
 		str++;
 	}
-	return 1;
+	return (1);
 }
-
+/**
+ * main - program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int i;
+	int sum = 0, i;
 
-	/* Check if no arguments are provided */
 	if (argc == 1)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
-
-	/* Iterate through arguments and calculate the sum */
 	for (i = 1; i < argc; i++)
 	{
-		/* Check if the argument is a valid positive integer */
 		if (!is_digit(argv[i]))
 		{
 			printf("Error\n");
-			return 1;
+			return (1);
 		}
-
-		/* Convert the argument to an integer and add to the sum */
 		sum += atoi(argv[i]);
 	}
-
-	/* Print the result */
 	printf("%d\n", sum);
-
-	return 0;
+	return (0);
 }
